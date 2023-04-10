adf9557e7b1de4f783de243832d0af2d82e16a79
Nikoloz Shervashidze <51024662+nsher888@users.noreply.github.com>
2023-04-10 21:59:12 +0400

    feat: add language switcher on admin panel pages

--- resources/views/components/languages.blade.php
+++ resources/views/components/languages.blade.php
@@ -1,4 +1,5 @@
-<div class="absolute left-6 top-[50%] flex flex-col gap-4">
+<div {{ $attributes->merge(['class' => 'absolute left-6 top-[50%] flex flex-col gap-4'])
+    }}>
     @foreach($available_locales as $locale_name => $available_locale)
     @if($available_locale === $current_locale)
     <div class="border-2 border-white rounded-full p-2">
@@ -6,7 +7,7 @@
     </div>
     @else
     <div class="border-2 border-white rounded-full p-2 text-black bg-white">
-        <a href="language/{{ $available_locale }}">{{ $locale_name }}</a>
+        <a href="{{ route('switchLanguage', $available_locale) }}">{{ $locale_name }}</a>
     </div>
     @endif
     @endforeach
