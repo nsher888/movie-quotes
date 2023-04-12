3c170d693e1abffcf3388860d676b78252b55e6d
Nikoloz Shervashidze <51024662+nsher888@users.noreply.github.com>
2023-04-13 02:33:36 +0400

    fix: align quote text left

--- resources/views/components/movie.blade.php
+++ resources/views/components/movie.blade.php
@@ -3,7 +3,7 @@
 <div class="border border-1 border-black bg-white w-[748px] rounded-xl overflow-hidden">
     <img src="{{ asset('storage/' . $quote->thumbnail) }}" class="max-h-96 w-full">
 
-    <div class="flex items-center h-28 border-t-1 border-black">
-        <p class="text-black text-4xl max-w-xl mx-auto">{{ $quote->text }}</p>
+    <div class="flex items-center h-28 border-t-1 border-black p-4">
+        <p class="text-black text-4xl max-w-xl">{{ $quote->text }}</p>
     </div>
 </div>
